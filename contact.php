e43ba9303eef77d64f4ba8e80f4788f7c346703c
Vivian Yu <33299524+vyu113@users.noreply.github.com>
2020-04-20 16:04:12 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'email';
+$from = 'info@vyu113.github.io';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'viviyu@umich.edu';
